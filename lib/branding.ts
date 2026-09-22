0cf5568e4518dd6df731b3d0c67a3435cd64c909
Pedro Augusto <102700254+PedroBrasil557@users.noreply.github.com>
2026-09-22 12:05:45 -0300

    brand: register official Cortex Ribbon assets

--- lib/branding.ts
+++ lib/branding.ts
@@ -2,4 +2,24 @@ export const BRAND = {
   name: 'Cérebro.IA',
   shortName: 'Cérebro',
   tagline: 'Decision Engine',
+  assets: {
+    mark: {
+      light: '/brand/cerebro-mark-light.svg',
+      dark: '/brand/cerebro-mark-dark.svg',
+      width: 84,
+      height: 76,
+    },
+    lockup: {
+      light: '/brand/cerebro-lockup-light.svg',
+      dark: '/brand/cerebro-lockup-dark.svg',
+      width: 250,
+      height: 76,
+    },
+    appIcon: {
+      light: '/brand/cerebro-app-icon-light.svg',
+      dark: '/brand/cerebro-app-icon-dark.svg',
+      width: 72,
+      height: 72,
+    },
+  },
 } as const
